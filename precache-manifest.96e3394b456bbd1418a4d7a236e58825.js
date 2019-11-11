8c0d0b169d49e31ed27f2c5941ed7d67ec221364
p-penchev <pavel.p.penchev@gmail.com>
2019-11-11 15:28:29 +0200

    Updates

---
+++ precache-manifest.96e3394b456bbd1418a4d7a236e58825.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c732997e6b897fb73294036d0588391e",
+    "url": "/albums-gallery/index.html"
+  },
+  {
+    "revision": "e68a106cb30660ba5509",
+    "url": "/albums-gallery/static/css/main.1eab7310.chunk.css"
+  },
+  {
+    "revision": "aea21a2608de9dd1b099",
+    "url": "/albums-gallery/static/js/2.def8b8a2.chunk.js"
+  },
+  {
+    "revision": "e68a106cb30660ba5509",
+    "url": "/albums-gallery/static/js/main.f075c8e0.chunk.js"
+  },
+  {
+    "revision": "46ce6552a2656fba7549",
+    "url": "/albums-gallery/static/js/runtime~main.7e98b712.js"
+  }
+]);
